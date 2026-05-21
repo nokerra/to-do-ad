92dae8bbadc9d99baa80082ffdeb9961a63dbb9c
nokerra <xnax019@gmail.com>
2026-05-21 19:52:24 +0300

    Законченный проект

--- index.js
+++ index.js
@@ -11,8 +11,24 @@ const listElement = document.querySelector(".to-do__list");
 const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
-function loadTasks() {
+formElement.addEventListener('submit', (event) => {
+	event.preventDefault();
 
+	listElement.prepend(createItem(inputElement.value));
+	items = getTasksFromDOM();
+	saveTasks(items);
+	inputElement.value = '';
+});
+
+function loadTasks() {
+	const savedTasks = localStorage.getItem('taskList');
+	
+	if (savedTasks) {
+		return JSON.parse(savedTasks);
+	} else {
+		saveTasks(items);
+		return items;
+	}
 }
 
 function createItem(item) {
@@ -23,13 +39,55 @@ function createItem(item) {
   const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
   const editButton = clone.querySelector(".to-do__item-button_type_edit");
 
+	textElement.textContent = item;
+	
+	deleteButton.addEventListener('click', (event) => {
+		clone.remove();
+
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+	duplicateButton.addEventListener('click', (event) => {
+		const itemName = textElement.textContent;
+		const newItem = createItem(item);
+		listElement.prepend(newItem);
+
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+	editButton.addEventListener('click', (event) => {
+		textElement.contentEditable = "true";
+		textElement.focus();
+	});
+
+	textElement.addEventListener('blur', (event) => {
+		textElement.contentEditable = "false";
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+	return clone;
 }
 
 function getTasksFromDOM() {
+	const itemsNamesElements = document.querySelectorAll(".to-do__item-text");
+	
+	let tasks = [];
+	itemsNamesElements.forEach((task) => {
+	tasks.push(task.textContent);
+	});
 
+	return tasks;
 }
 
 function saveTasks(tasks) {
-
+	localStorage.setItem('taskList', JSON.stringify(tasks));
 }
 
+items = loadTasks();
+
+items.forEach((task) => {
+	listElement.append(createItem(task));
+});
